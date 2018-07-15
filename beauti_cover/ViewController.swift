7b901250677984b0de95ce64dd612f153398b084
Your Name <you@example.com>
2018-07-15 21:24:05 +0800

    Initial Commit

---
+++ beauti_cover/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beauti_cover
+//
+//  Created by Justin Lin on 2018/7/15.
+//  Copyright © 2018年 Justin Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
